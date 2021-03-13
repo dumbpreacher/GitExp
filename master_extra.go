a2db5b62dbd2c6d95d4d8b84b92ffaf2df0cb940
swagat.parida <swagat.parida@go-jek.com>
2021-03-13 10:47:47 +0530

    5 master

--- master_extra.go
+++ master_extra.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func Master() {
 	fmt.Println("master extra commit - 4 (after .idea remove)")
+	fmt.Println("In this commit, exp-rebase has already branched out and this is the only commit before the git rebase")
 }
