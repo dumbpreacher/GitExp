554c28ea507c074e0dc368cc6d9ac92094ceb98c
swagat.parida <swagat.parida@go-jek.com>
2021-03-13 12:02:16 +0530

    clean main

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("first commit on master")
-	fmt.Println("second commit on master")
-	fmt.Println("third commit on master")
-	fmt.Println("commit on master")
 }
 
 //comment to check merge
